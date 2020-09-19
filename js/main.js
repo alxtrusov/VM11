77a11622b46ca041836d8a31149988eff9f383ee
Alexey <alxtrusov@gmail.com>
2020-09-19 12:31:56 +0400

    set first version of RPG

--- js/main.js
+++ js/main.js
@@ -1,47 +1,45 @@
 
 'https://github.com/'
 
-var r = 15, 
-    x = prompt('Введи Х') - 0, 
-    y = prompt('Введи Y') - 0;
+var rooms = [
+    [
+        'коридор',
+        'Перед тобой дверь в столовую и дверь в аудиторию. Куда пойдешь?',
+        ['налево', 'да', 'направо', 'нет'],
+        [1, 1, 1, 3],
+        null
+    ], [
+        'столовка',
+        'Ты обожрался. Хочешь ещё поесть, или пойдешь учиться?',
+        ['да', 'нет', 'хочуучиться!'],
+        [1, 1, 0],
+        1
+    ], 'аудитория', 'туалет', 'военкомат'
+];
+var step = 0;
 
-if (isNaN(x) || isNaN(y)) {
-    alert('Введены нечисленные значения!');
-} else {
-    // стреляем в кружок
-    if (Math.sqrt(x*x + y*y) <= r) {
-        alert('Попал!');
-    } else {
-        alert('Промазал!!!');
+while (1) {
+    if (step === null) {
+        break;
+    }
+    var room = rooms[step];
+    if (!room) {
+        break;
+    }
+    var answer = prompt(room[1]);
+    if (!answer) {
+        continue;
+    }
+    answer = answer.toLowerCase().replace(' ', '');
+    var isWayNotFound = true;
+    for (var i = 0; i < room[2].length; i++) {
+        if (answer === room[2][i]) {
+            step = room[3][i];
+            isWayNotFound = false;
+            break;
+        }
+    }
+    if (isWayNotFound) {
+        step = room[4];
     }
 }
-// залить монохромным цветом 
-var canvas = document.getElementById('canvas');
-var context = canvas.getContext('2d');
-context.fillStyle = 'yellow';
-context.fillRect(0, 0, canvas.width, canvas.height);
-// нарисовать мишень
-context.beginPath();
-context.strokeStyle = 'green'; // цвет линии
-context.arc(canvas.width/2, canvas.height/2, canvas.width/2, 0, 2*Math.PI);
-context.stroke();
-// нарисовать координатные оси
-context.beginPath();
-context.strokeStyle = 'black'; // цвет линии
-// Oy
-context.moveTo(canvas.width/2, 0);
-context.lineTo(canvas.width/2, canvas.height);
-// Ox
-context.moveTo(0, canvas.height/2);
-context.lineTo(canvas.width, canvas.height/2);
-context.stroke();
-// нарисовать результат попадание
-context.beginPath();
-context.fillStyle = 'red'; // цвет заливки
-context.strokeStyle = 'red'; // цвет линии
-context.arc(canvas.width/2 + x, canvas.height/2 + y, 10, 0, 2*Math.PI);
-context.fill();
-
-// стреляем в квадрат
-// стреляем в ромб
-// стреляем в прямоугольник
